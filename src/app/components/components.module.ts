3be6142f2601b21c70ba65a9e863f934a7ad80b8
satyamsinha111 <satyamsinha8158@gmail.com>
2022-01-09 22:21:36 +0530

    pushed

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { CreateProjectComponent } from './create-project/create-project.component';
 
-
+const customComponents = [CreateTaskComponent, CreateProjectComponent];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [customComponents],
+  imports: [CommonModule],
+  exports: [customComponents],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
--- src/app/pages/dashboard/pages/tasks/tasks.component.ts
+++ src/app/pages/dashboard/pages/tasks/tasks.component.ts
@@ -1,15 +1,18 @@
+import { CreateTaskComponent } from './../../../../components/create-task/create-task.component';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.scss']
+  styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
+  constructor(private _dialog: MatDialog) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  addTask() {
+    this._dialog.open(CreateTaskComponent);
   }
-
 }
